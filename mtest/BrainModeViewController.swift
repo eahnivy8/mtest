aaa38f513d8958670487a2f6e14da8db45ba2688
edahn7 <edahn7@bodyfriend.co.kr>
2022-04-03 17:56:57 +0900

    브레인파일추가

---
+++ mtest/BrainModeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BrainModeViewController.swift
+//  mtest
+//
+//  Created by SWAhn on 2022/04/03.
+//
+
+import UIKit
+
+class BrainModeViewController: UIViewController {
+    var brain = 000000
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
