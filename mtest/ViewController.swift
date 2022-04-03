782451263c663ce612783fd5170a688c94a1a6a8
edahn7 <edahn7@bodyfriend.co.kr>
2022-04-03 17:53:52 +0900

    brainmode 변수 추가

--- mtest/ViewController.swift
+++ mtest/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     var number = 12345
-    
+    var brainmode = "brain mode"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
